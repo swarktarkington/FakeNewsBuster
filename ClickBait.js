da605e0a1ca50b4dbbd825f1f3c46016c667947b
Jack <jack.philip.butler@gmail.com>
2017-02-20 12:51:24 +0000

    adding some initial text files to outline behaviour and ideas

---
+++ ClickBait.js
@@ -0,0 +1,24 @@
+//TacklingClickBait
+/*
+    This section aims to tackle clickbait, by simply identifying some of the formulaic
+    patterns that clickbait titles follow. Anything clickbait will be an amber flag.
+
+    Example easily spotted clickbaits 
+    Restore [pronoun]'s faith in humanity
+    this
+    You wont beleive what happens next
+    You wont beleive
+    What happens next
+    
+    One [adjective] [noun]
+    Number [x] is a[n] [verb]
+    will surprise you
+    will shock you
+    will blow you away
+    unknown
+    one weird trick
+    [scientist/doctor/pharmacist/journalist]'s hate [pronoun]
+    
+    Listicle detection  
+ */
+
---
+++ WebsiteFiltering.js
@@ -0,0 +1,20 @@
+/* 
+    untrustworthy websites
+
+    immediate mark:
+    -4chan
+    -upworthy
+    -buzzfeed
+    -breitbart | literal nazi website
+    -clickhole | mention it is satire, explain satire
+    -theonion  | mention it is satire, explain satire
+    -townhall
+    -gawker
+
+    warning mark:
+    things linked from reddit get a warning
+
+    
+
+
+*/
